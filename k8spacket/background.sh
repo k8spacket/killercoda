c462185711e7754b00f561dcf6f9824a936cfff8
k8spacket <k8spacket@gmail.com>
2024-02-06 22:23:33 +0100

    killercoda

--- k8spacket/background.sh
+++ k8spacket/background.sh
@@ -2,9 +2,17 @@
 
 set -x # to test stderr output in /var/log/killercoda
 
+kubectl taint node controlplane node-role.kubernetes.io/control-plane:NoSchedule-
+
 helm repo add k8spacket https://k8spacket.github.io/k8spacket-helm-chart
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
 helm install k8spacket --namespace k8spacket k8spacket/k8spacket --create-namespace
+helm upgrade --install -n monitoring promop prometheus-community/kube-prometheus-stack --set grafana.adminPassword=admin -f ./promop-values.yaml
+
+kubectl -n monitoring apply --recursive -f ./dashboards
+
+kubectl port-forward --address 0.0.0.0 service/k8spacket 8080:8080
 
 touch /tmp/finished
--- k8spacket/intro.md
+++ k8spacket/intro.md
@@ -1,3 +1,7 @@
 <br>
 
 ### Let's learn some Kubernetes - k8spacket
+
+[Grafana]({{TRAFFIC_HOST1_31400}})
+
+[Prometheus]({{TRAFFIC_HOST1_31401}})
