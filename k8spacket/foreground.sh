f893514e3c6d89b30a44f0c9d8d0c2b76f368c2f
k8spacket <k8spacket@gmail.com>
2024-04-03 20:26:20 +0200

    k8spacket

--- k8spacket/foreground.sh
+++ k8spacket/foreground.sh
@@ -1,3 +1,6 @@
 echo "Installing tools..."
 while [ ! -f /tmp/finished ]; do sleep 1; done
-echo DONE
+
+kubectl -n monitoring wait --for=condition=Ready pod -l app.kubernetes.io/name=grafana
+
+echo "Everything is ready"
