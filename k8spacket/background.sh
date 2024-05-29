ca36c2979e3188c377affddb3d261223a236d0c6
k8spacket <k8spacket@gmail.com>
2024-05-29 16:39:20 +0200

    fix

--- k8spacket/background.sh
+++ k8spacket/background.sh
@@ -8,7 +8,7 @@ helm repo add k8spacket https://k8spacket.github.io/k8spacket-helm-chart
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-helm install k8spacket --namespace k8spacket k8spacket/k8spacket --create-namespace --set image.tag="kernel5.4a"
+helm install k8spacket --namespace k8spacket k8spacket/k8spacket --create-namespace --set image.tag="kernel5.4a" --version 2.0.2
 
 helm install promop --namespace monitoring prometheus-community/kube-prometheus-stack --create-namespace -f ./promop-values.yaml
 
