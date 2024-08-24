b29dc15cce5b25b5e931fc70ca2add1b1d49549d
k8spacket <k8spacket@gmail.com>
2024-08-24 12:04:34 +0200

    version to 2.0.5

--- k8spacket/background.sh
+++ k8spacket/background.sh
@@ -9,14 +9,14 @@ kubectl create namespace init
 kubectl create namespace monitoring
 
 helm install promop --namespace monitoring prometheus-community/kube-prometheus-stack --create-namespace -f ./promop-values.yaml \
-  && kubectl -n monitoring apply --recursive -f ./dashboards &
+  && kubectl -n monitoring apply --recursive -f ./dashboards \
+  && kubectl -n monitoring wait --for=condition=Ready pod -l app.kubernetes.io/name=grafana &
 
 kubectl taint node controlplane node-role.kubernetes.io/control-plane:NoSchedule- \
   && helm install k8spacket --namespace k8spacket k8spacket/k8spacket --create-namespace --set image.tag="2.0.5-kernel5.4a" --version 2.0.5 \
   && kubectl taint node controlplane node-role.kubernetes.io/control-plane:NoSchedule \
-  && kubectl apply -f python-app.k8s.yaml &
+  && kubectl apply -f python-app.k8s.yaml \
+  && kubectl -n init wait --for=condition=complete job/initial-curls &
 
-kubectl -n monitoring wait --for=condition=Ready pod -l app.kubernetes.io/name=grafana
-kubectl -n init wait --for=condition=complete job/initial-curls
-
+wait < <(jobs -p)
 touch /tmp/finished
