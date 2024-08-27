17465a2d71266ec21188648675c15d099ebfd061
k8spacket <k8spacket@gmail.com>
2024-08-27 08:10:09 +0200

    2.0.6 chart

--- k8spacket/background.sh
+++ k8spacket/background.sh
@@ -10,7 +10,12 @@ helm install promop --namespace monitoring prometheus-community/kube-prometheus-
   && kubectl -n monitoring wait --for=condition=Ready pod -l app.kubernetes.io/name=grafana &
 
 kubectl taint node controlplane node-role.kubernetes.io/control-plane:NoSchedule- \
-  && helm install k8spacket --namespace k8spacket k8spacket/k8spacket --create-namespace --set image.tag="2.0.5-kernel5.4a" --version 2.0.5 \
+  && helm install k8spacket --namespace k8spacket k8spacket/k8spacket --create-namespace --version 2.0.6 \
+     --set image.tag="2.0.5-kernel5.4a" \
+     --set serviceMonitor.enabled="true" \
+     --set containerPorts[0].name="metrics" \
+     --set containerPorts[0].containerPort="6676" \
+     --set containerPorts[0].protocol="TCP" \
   && kubectl apply -f python-app.k8s.yaml \
   && kubectl -n init wait --for=condition=complete job/initial-curls &
 
